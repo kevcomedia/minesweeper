f1cb28937aa27340d9636753cfe7561383c0819e
Kev Comedia <kevcomedia@gmail.com>
2021-05-04 14:52:48 +0800

    Write board seed logic

--- board.rb
+++ board.rb
@@ -1,7 +1,19 @@
+require_relative 'tile'
+
 class Board
   SIZE = 9
+  BOMBS = 10
 
   def initialize
     @board = nil
+  end
+
+  def seed
+    bomb_locations = (0...SIZE**2).to_a.sample(BOMBS)
+    tiles = (0...SIZE**2).map do |i|
+      has_bomb = bomb_locations.include?(i)
+      Tile.new(has_bomb)
+    end
+    @board = (0...SIZE).map { |i| tiles[i * SIZE...(i + 1) * SIZE] }
   end
 end
