3876b8d60352059fb0cd009ad46372139175752f
Kev Comedia <kevcomedia@gmail.com>
2021-05-04 15:34:26 +0800

    Write tile flagging logic

--- tile.rb
+++ tile.rb
@@ -1,16 +1,26 @@
 class Tile
-  attr_reader :bomb, :revealed
+  attr_reader :bomb, :revealed, :flagged
 
   def initialize(bomb)
     @bomb = bomb
     @revealed = false
+    @flagged = false
   end
 
   def reveal
-    @revealed = true
+    @revealed = true unless @flagged
+  end
+
+  def toggle_flag
+    if @flagged
+      @flagged = false
+    else
+      @flagged = true unless @revealed
+    end
   end
 
   def to_s
+    return 'F' if @flagged
     return '*' unless @revealed
     @bomb ? '@' : '_'
   end
