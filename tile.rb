164750bceafb460ac5dc52220bb4cfb877521dc5
Kev Comedia <kevcomedia@gmail.com>
2021-05-10 14:18:25 +0800

    Add tile colors

--- tile.rb
+++ tile.rb
@@ -1,6 +1,19 @@
+require 'colorize'
+
 class Tile
   attr_reader :bomb, :revealed, :flagged
   attr_accessor :neighbors
+
+  BOMB_HINT_COLORS = {
+    1 => :light_blue,
+    2 => :green,
+    3 => :light_red,
+    4 => :blue,
+    5 => :magenta,
+    6 => :cyan,
+    7 => :light_black,
+    8 => :light_black,
+  }
 
   def initialize(bomb)
     @bomb = bomb
@@ -22,11 +35,15 @@ class Tile
   end
 
   def to_s
-    return 'F' if @flagged
+    return 'F'.red.on_white if @flagged
     return '*' unless @revealed
-    return '@' if @bomb
+    return '@'.black.on_red if @bomb
+
     neighbor_bomb_count = self.neighbor_bomb_count
-    neighbor_bomb_count > 0 ? neighbor_bomb_count.to_s : '_'
+    return '_'.light_black if neighbor_bomb_count == 0
+
+    color = BOMB_HINT_COLORS[neighbor_bomb_count]
+    neighbor_bomb_count.to_s.colorize(color)
   end
 
   def neighbor_bomb_count
