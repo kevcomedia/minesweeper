79374cd1fb86db344c2e20bc053dc14611261691
Kev Comedia <kevcomedia@gmail.com>
2021-05-10 17:14:11 +0800

    Allow saving

--- game.rb
+++ game.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require_relative 'board'
 
 class Game
@@ -25,29 +26,42 @@ class Game
     end
   end
 
+  def save
+    serialized_board = @board.to_yaml
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+    File.open('./saves/save', 'w+') { |f| f.write(serialized_board) }
+  end
+
   def prompt
     puts
     puts "Enter an action, a row and column (e.g., 'r 2 5')"
     puts "Actions:"
     puts " r - reveal"
     puts " f - flag"
+    puts " s - save"
     action, *pos = gets.chomp.downcase.split
-    raise "invalid action" unless action == 'r' || action == 'f'
+    [action, pos]
+  end
+
+  def parse_pos(pos)
     raise "invalid pos" unless pos.length == 2
     begin
-      pos = pos.map { |p| Integer(p) }
+      pos.map { |p| Integer(p) }
     rescue
       raise "invalid pos"
     end
-    [action, pos]
   end
 
   def do_action(action, pos)
     case action
     when 'r'
-      @board.reveal(pos)
+      @board.reveal(parse_pos(pos))
     when 'f'
-      @board.toggle_flag(pos)
+      @board.toggle_flag(parse_pos(pos))
+    when 's'
+      save
+    else
+      raise 'invalid action'
     end
   end
 end
